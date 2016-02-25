812851317a80521f25f78e1b69daadb065a5fcc2
Ben Hughes <ben@Bens-MacBook.local>
2016-02-25 21:48:13 +0000

    Configurable keymap

--- client.py
+++ client.py
@@ -1,30 +1,39 @@
 """Client for toggling lights."""
+import os
 import time
+import redis
 import logging
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
+
+
+r = redis.Redis(os.getenv('REDIS_URL'))
+client_map = r.hgetall('keymap')
+
 
 try:
     from energenie import switch_on, switch_off
 except ImportError:
     log.error("No energenie installed.")
-    switch_on = lambda x: x
-    switch_off = lambda x: x
 
+    def switch_on(num):
+        """Dummy function."""
+        pass
 
-client_map = {'kitchen': 2, 'lounge': 3}
+    def switch_off(num):
+        """Dummy function."""
+        pass
 
 
 def toggle_lights(state, room):
     """Toggle lights."""
     log.info("Toggling lights in {}".format(room))
-    # TODO - put in energenie code.
 
     for attempt in range(3):
         log.info("Attempt {} of 3: Sending '{}' command to switch {}..."
-                 .format(attempt, state, sock))
+                 .format(attempt, state))
         if state == 'on':
-            switch_on(sock)
+            switch_on(client_map.get(room))
         elif state == 'off':
-            switch_off(sock)
+            switch_off(client_map.get(room))
         time.sleep(1)
